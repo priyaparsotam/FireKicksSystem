78d9dc874a108da1893b654ac9a5618db69100a6
p17238326 <p17238326@GWHXX472WIN004W>
2020-03-02 14:15:40 +0000

    updated new branch

---
+++ FireKicksClasses/clsOrderProcessing.cs
@@ -0,0 +1,9 @@
+﻿namespace FireKicksClasses
+{
+    public class clsOrderProcessing
+    {
+       
+        public bool Dispatched { get; set; }
+        public int OrderID { get; set; }
+    }
+}
---
+++ FireKicksTesting/tstOrderProcessing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FireKicksClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FireKicksTesting
+{
+    [TestClass]
+    public class tstOrderProcessing
+    {
+
+
+
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsOrderProcessing AnOrderProcessing = new clsOrderProcessing();
+            Assert.IsNotNull(AnOrderProcessing);
+        }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrderProcessing AnOrderProcessing = new clsOrderProcessing();
+            int TestData = 1;
+            AnOrderProcessing.OrderID = TestData;
+            Assert.AreEqual(AnOrderProcessing.OrderID, TestData);
+        }
+
+        [TestMethod]
+
+
+
+        [TestMethod]
+        public void DispatchedPropertyOk()
+        {
+            clsOrderProcessing AnOrderProcessing = new clsOrderProcessing();
+            Boolean TestData = true;
+            AnOrderProcessing.Dispatched = TestData;
+            Assert.AreEqual(AnOrderProcessing.Dispatched, TestData);
+
+        }
+    }
+}
