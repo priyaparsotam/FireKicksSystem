afc65837db949333b062dfde003eed6bec1a9f9e
p17238326 <p17238326@GWHXX472WIN004W>
2020-03-03 14:09:08 +0000

    Changes

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿USE [Firekicks]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_tblOrderProcessing_FilterbyOrderID]
+		@OrderID = NULL
+
+SELECT	@return_value as 'Return Value'
+
+GO
