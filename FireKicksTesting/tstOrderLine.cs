bb067d390cbc45a47a85cf6f7727fca85fd22b64
p17238326 <p17238326@GWHXX472WIN004W>
2020-03-10 14:51:38 +0000

    Started OrderLine

---
+++ FireKicksTesting/tstOrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FireKicksTesting
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Assert.IsNotNull(AnOrderLine)
+
+
+        }
+    }
+}
